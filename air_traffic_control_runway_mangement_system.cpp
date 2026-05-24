48802704593e7b27ee7217989955c1813e62ac64
Jashan <jashansindher855@gmail.com>
2026-05-24 15:40:33 +0530

    Air traffic control(ATC) and runway management system
    🛬ATC & Runway Management System (C++)
    ​Developed a smart Air Traffic Control (ATC) & Runway Management System designed to automate and prioritize aircraft landings based on real-time situational urgency.
    ​🚀 Key Features & Logic Implemented:
    ​Landing Classification: System dynamically handles inputs for both Normal and Emergency landing scenarios.
    ​Smart Emergency Triage:
    ​Medical Emergency: Instantly routes and assigns the aircraft to the runway nearest to medical/ambulance stations.
    ​Technical Fault: Automatically allocates the runway closest to the maintenance hangar/garage for immediate inspection.
    ​Intelligent Fuel Shortage Handler: * < 40% Fuel: Triggers an Immediate Landing protocol due to critical fuel levels.
    ​40% - 80% Fuel: Scans and suggests the most optimal available runway.
    ​Boundary Validation: Strict error-handling that flags any inputs outside the 1%–99% range as an "Invalid Fuel Count" to prevent data corruption.
    ​🛠️ Tech Stack:
    ​C++ (Core Logic, Conditional Matrix
    #Coding journey #C++

---
+++ air_traffic_control_runway_mangement_system.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    char choice;
+    cout<<"Which type of landing is it \nA. normal[n] \nB. Emergency[e]"<<endl;
+    cin>>choice;
+
+    if(choice == 'e'){
+
+        cout<<"which type of Emergency"<<endl;
+        cout<<"medical A. emergency[m] \nB. shortage of fuel[f] \nC. technical fault[t]"<<endl;
+
+        char choice;
+        cin>>choice;
+
+        if(choice == 'm'){
+
+            cout<<"assigning you Runway closest to ambulance"<<endl;
+            cout<<" you can start landing process"<<endl;
+
+        }else if(choice == 't'){
+
+             cout<<"assigning you Runway closest to techanical team/garage"<<endl;
+             cout<<" you can start landing process"<<endl;
+
+        }else if(choice == 'f'){
+
+           cout<<"how much fuel is left"<<endl;
+           int fuel_left;
+           cin>>fuel_left;
+
+           if(fuel_left > 100 || fuel_left < 0){
+
+            cout<<" please enter valid count!"<<endl;
+
+        }else if(fuel_left < 40){
+
+            cout<<"land imediately!!"<<endl;
+
+           }else if(fuel_left >= 40 && fuel_left < 80){
+
+            cout<<"wait we are assigning you a runway"<<endl;
+           }
+
+        }
+
+    } else if(choice == 'n'){
+
+        cout<<"wait we are assigning you a runway"<<endl;
+
+    }else{
+
+        cout<<"enter valid options "<<endl;
+    }
+
+}
