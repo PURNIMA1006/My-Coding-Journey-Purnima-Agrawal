a9726790a2e1797c259381c02d145ba9cd328076
Jashan <jashansindher855@gmail.com>
2026-05-27 18:31:03 +0530

    Address record
    Address Record System
    
    A simple C++ project to store and manage address records using arrays and input validation.
    
    ---
    
    🎯 Features
    
    - Input validation using "if-else"
    - Uses arrays for structured data storage
    - Unique pre-assigned codes for records
    
    ---
    
    🛠️ Tech Stack
    
    - C++
    - Arrays
    - If-Else
    
    ---
    
    🚀 Learning Outcome
    
    Through this project, I practiced:
    
    - working with arrays
    - handling structured input
    - improving code readability
    - implementing validation logic
    
    ---
    
    #CodingJourney
     #C++

---
+++ address_record.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int address_list[4] = {101 , 102 , 103 , 104};
+    int choice; 
+    cout<<"enter address code \n **lie between 0 to 3**"<<endl;
+    cin>>choice;
+
+
+    if(choice == 0){
+
+        cout<<"Owner  name - X \nfloor = G \nhouse number = 1 \nfull address = X 1G \nunique code"<<address_list[0]<<endl;
+        return 0;
+
+    }else if(choice == 1){
+
+        cout<<"Owner  name - Y \nfloor = G \nhouse number = 2 \nfull address = Y 2G \nunique code"<<address_list[1]<<endl;
+        return 0;
+
+    }else if(choice == 2){
+
+        cout<<"Owner  name - Z \nfloor = G \nhouse number = 3 \nfull address = Z 3G \nunique code"<<address_list[2]<<endl;
+        return 0;
+    }else if(choice == 3){
+
+        cout<<"Owner  name - H \nfloor = G \nhouse number = 4 \nfull address = H 4G \nunique code"<<address_list[3]<<endl;
+        return 0;
+    }
+}
